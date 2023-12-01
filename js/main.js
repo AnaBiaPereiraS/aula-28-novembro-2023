5e31b14c1a8ed3e871f5e3a40a08e79327f66c87
Ana Beatriz <ana.b.sousa22@aluno.senai.br>
2023-11-30 21:30:25 -0300

    criação de grafico

--- js/main.js
+++ js/main.js
@@ -16,14 +16,11 @@ let alunos = [
   
 ];
 
-// carregar os dados do array na tabela
 window.onload = (event) => {
-    let tbody = document.querySelector("#myTable");
+    let tbody = document.querySelector("#tBody");
 alunos.forEach((aluno, index) => {
     let tr = document.createElement("tr");
-
-    tr.id = index + 1
-
+    tr.id = index +1
     let tdCod = document.createElement("td");
     let tdNome = document.createElement("td");
     let tdEmail = document.createElement("td");
@@ -36,7 +33,7 @@ alunos.forEach((aluno, index) => {
     tdEmail.textContent = aluno.email;
     tdTelefone.textContent = aluno.telefone;
     tdProfissao.textContent = aluno.profissao;
-    tdBotao.innerHTML = `<button class="btn-remover" onclick="remover(this)" >Remover</button>`
+    tdBotao.innerHTML = `<button class="btn btn-danger" onclick="remover(this)" >Remover</button>`
 
     tr.appendChild(tdCod);
     tr.appendChild(tdNome);
@@ -46,18 +43,19 @@ alunos.forEach((aluno, index) => {
     tr.appendChild(tdBotao);
 
     tbody.appendChild(tr);
-
-
 });
+
+initPieChart();
 }
-//
+
 function remover (id){
     let row = id.parentNode.parentNode.id;
     row = document.getElementById(row);
     row.parentNode.removeChild(row);
     return false
+ 
+ 
 }
-
 function adicionar(){
     //Definindo as variaveis e recebendo os dados
  let nome = document.getElementById('nome').value;
@@ -82,7 +80,7 @@ function adicionar(){
  row.id = tableSize;
 
  //Criando o codigo do botão para remover a linha
- let btnCode = "<button class='remove-btn' onclick='remover(this)'>Remover</button>";
+ let btnCode = "<button class='btn btn-danger' onclick='remover(this)'>Remover</button>";
  //Preenchendo as celulas da linha
  col1.innerHTML = tableSize;
  col2.innerHTML = nome;
@@ -99,6 +97,58 @@ function adicionar(){
 
 //Retornando 'false' para impedir o reload da pagina
  return false
+};
 
-}
+const initPieChart = () => {
+    const ctx = document.getElementById("myChart");
+    const data = {
+        labels: ['Frontend', 'Backend', 'Fullstack','Mobile'],
+        datasets: [
+            {
+                label: 'Quantidade',
+                data: [10, 20, 50, 20],
+                backgroundColor:  ['pink', 'lightblue', 'lightgreen', 'orange'],
+                hoverOffset: 4
+            }
+        ]
+    }
 
+    const pieChart = new Chart(
+        ctx,
+        {
+            type: 'pie',
+            data: data
+        }
+    );
+};
+const initPolarAreaChart = () => {
+    removerChart();
+    // 1 passo: pegar o elemento #myChart
+    const ctx = document.getElementById('myChart');
+
+    //2 passo: definir os dados do grafico
+    const data = {
+        labels: ['Frontend', 'Backend', 'Fullstack','Mobile'],
+        datasets: [{
+            label: 'Quantidade',
+            data: [ 11, 15, 7, 3],
+            backgroundColor: ['pink', 'lightblue', 'lightgreen', 'orange']
+        }]
+    };
+
+    const polarAreaChart = new Chart(
+        ctx,
+        {
+            type: 'polarArea',
+            data: data
+        }
+    );
+};
+
+const removerChart = () => {
+    const divChart = document.querySelector('.chart');
+    document.getElementById("myChart").remove();
+    const myChart = document.createElement("canvas");
+    myChart.id = "myChart";
+    divChart.appendChild(myChart);
+};
